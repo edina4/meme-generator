1bfcfad8b68559fb8a8402d2aefc966ca438fbea
Suller Edina <esuller236@gmail.com>
2023-03-01 15:08:14 +0200

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+export default function Navbar(){
+    return(
+        <div className="nav">
+            <img src="../images/trollface.png" className="troll"/>
+            <h1 className="nav-txt">Meme Generator</h1>
+        </div>
+    )
+}
